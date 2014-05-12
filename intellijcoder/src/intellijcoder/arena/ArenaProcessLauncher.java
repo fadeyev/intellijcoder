7b632739d17c59a9efb8bbda7e45372a763fed36
mmicheo <chivo@usa.net>
2014-05-11 23:43:28 -0500

    Added capability to launch arena with a remote debugging port open from within the IDE.  The launcher to test the intellijcoder plugin can receive a vm parameter -Dintellijcoder.arena.debug.port=12345, that will in turn be passed as the remote debugging port for the arena.  To debug, just create a remote debugger with the same port number.

--- intellijcoder/src/intellijcoder/arena/ArenaProcessLauncher.java
+++ intellijcoder/src/intellijcoder/arena/ArenaProcessLauncher.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 /**
  * Starts TopCoder application in new process
  *
@@ -18,6 +16,7 @@ import static java.util.Arrays.asList;
  */
 public class ArenaProcessLauncher {
     public static final String INTELLIJCODER_PORT_PROPERTY = "intellijcoder.port";
+    public static final String INTELLIJCODER_DEBUG_PORT_PROPERTY = "intellijcoder.arena.debug.port";
     public static final String FAILED_TO_START_PROCESS_MESSAGE = "Failed to start TopCoder application process";
 
     private ProcessLauncher processLauncher;
@@ -29,10 +28,20 @@ public class ArenaProcessLauncher {
     public void launch(ArenaAppletInfo appletInfo, int port) throws IntelliJCoderException {
         try {
             String javaExecutable = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-            List<String> arguments = new ArrayList<String>(asList(javaExecutable,
-                                "-cp", appletInfo.getClassPath(),
-                                "-D" + INTELLIJCODER_PORT_PROPERTY + "=" + port,
-                                appletInfo.getMainClass()));
+            List<String> arguments = new ArrayList<String>();
+            arguments.add(javaExecutable);
+            arguments.add("-cp");
+            arguments.add(appletInfo.getClassPath());
+            arguments.add("-D" + INTELLIJCODER_PORT_PROPERTY + "=" + port);
+
+            Integer debugPort = Integer.getInteger(INTELLIJCODER_DEBUG_PORT_PROPERTY);
+            if (debugPort != null) {
+                arguments.add("-Xdebug");
+                arguments.add("-Xnoagent");
+                arguments.add("-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=" + debugPort);
+            }
+
+            arguments.add(appletInfo.getMainClass());
             arguments.addAll(appletInfo.getArguments());
 
             processLauncher.launch(arguments.toArray(new String[arguments.size()]));
